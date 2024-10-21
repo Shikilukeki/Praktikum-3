f2ff2f001590ecd154dce836f86609906c9ad5c5
Rifqi Maulana <maulanarifqi428@gmail.com>
2024-10-21 15:58:54 +0700

    Update Program perulangan input untuk mencari bilangan terbesar.py

--- Program perulangan input untuk mencari bilangan terbesar.py
+++ Program perulangan input untuk mencari bilangan terbesar.py
@@ -1,11 +1,9 @@
-def find_max():
-    MAX = int('0')
-    while True:
-        x = int(input("Inputkan Bilangan x : "))
-        if x == 0:
-            break
-        if x > MAX:
-            MAX = x
-    print("Bilangan terbesar adalah :", MAX)
+MAX = int('0')
 
-find_max()
+while True:
+    x = int(input("Inputkan Bilangan x : "))
+    if x == 0:
+        break
+    if x > MAX:
+        MAX = x
+print("Bilangan terbesar adalah :", MAX)
